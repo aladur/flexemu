ac06aaa7fd79d05d9ee71da1a927ca2e5e2f2f45
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2021-03-12 19:58:09 +0100

    Fix indentation.

--- src/cvtwchar.cpp
+++ src/cvtwchar.cpp
@@ -1,23 +1,23 @@
 /*
-cvtwchar.cpp
+    cvtwchar.cpp
 
 
-FLEXplorer, An explorer for any FLEX file or disk container
-Copyright (C) 2016-2021 W. Schwotzer
+    FLEXplorer, An explorer for any FLEX file or disk container
+    Copyright (C) 2016-2021 W. Schwotzer
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #ifdef _WIN32
--- src/sguiopts.h
+++ src/sguiopts.h
@@ -1,23 +1,23 @@
 /*
-sguiopts.h
+    sguiopts.h
 
 
-flexemu, an MC6809 emulator running FLEX
-Copyright (C) 2018-2021  W. Schwotzer
+    flexemu, an MC6809 emulator running FLEX
+    Copyright (C) 2018-2021  W. Schwotzer
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 
