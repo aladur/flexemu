419f3d9da288a348fa059ae3705302bd0dd0ffc8
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2021-09-07 18:55:32 +0200

    autotools: Support precompiled header.
    - This reduces buid time on Unix/Linux for about 23%

---
+++ src/pch.h
@@ -0,0 +1,57 @@
+/*
+    pch.h
+
+
+    flexemu, an MC6809 emulator running FLEX
+    Copyright (C) 2021  W. Schwotzer
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#ifndef PCH_INCLUDED
+#define PCH_INCLUDED
+
+#include "misc1.h"
+
+#include <ctype.h>
+#include <stdio.h>
+
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <functional>
+#include <utility>
+#include <cctype>
+#include <cassert>
+#include <locale>
+#include <iomanip>
+#include <exception>
+#include <stdexcept>
+#include <new>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <array>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <ostream>
+#include <fstream>
+#include <iostream>
+
+#endif
+
