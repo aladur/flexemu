6a8383472dc3a5ee3db605481874d42643955e2f
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2022-08-30 22:06:58 +0200

    Windows: Improve build scripts
    - download_and_rebuild_libs.sh:
      - Support Microsoft Visual Studio 2019 and 2022
      - Support Qt5 (incl. opensource versions >= 5.15.3) and Qt6
      - Script is more verbose
    - rebuild_flexemu_all_and_copy_libs.sh:
      - Automatically get Microsoft Visual Studio Version
        (see file vsversion.ini)
      - Automatically get Qt version from file msvcQtPath.props
      - Script is more verbose
    - Some general remarks:
      - Qt6 only supports x64 builds (no more Win32 support)
      - The flexemu solution and projects do not yet directly support
        to build with Qt6. The sources can be build with Qt6.

--- build/windows/download_and_rebuild_libs.sh
+++ build/windows/download_and_rebuild_libs.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/sh
 #
-# Download and rebuild Qt 5.x.y libraries needed to build flexemu
+# Download and rebuild Qt x.y.z libraries needed to build flexemu
 #
 # syntax:
-#    download_and_rebuild_libs_qt5.sh [-d][-V <vs_version][-T <vs_type>]
-#                                     -v <qt_version>
+#    download_and_rebuild_libs.sh [-d][-V <vs_version][-T <vs_type>]
+#                                 -v <qt_version>
 # parameters:
 #   -d:             Delete packages and directories before downloading
 #                   and building them.
-#   -V <vs_version> The Visual Studio version, 2017 or 2019.
+#   -V <vs_version> The Visual Studio version, 2019 or 2022.
 #                   If not set the script looks for an installed VS version
-#                   2019 or 2017, in this order.
+#                   2019 or 2022, in this order. Visual Studio has to be 
+#                   installed in the default path.
 #   -T <vs_type>    The Visual Studio type, Enterprise, Professional or
 #                   Community. If not set the script looks for an installed VS
 #                   type Enterprise, Professional or Community, in this order.
 #   -v qt_version   Specify Qt version to be build.
 #                   Syntax: <major>.<minor>.<patch>
+#                   Supported major version is 5 or 6.
+#                   See https://download.qt.io/archive/qt/ for available versions.
 #
+# Qt5
+#=====
 # Prerequisites to build Qt on Windows see:
 #    https://doc.qt.io/qt-5/windows-requirements.html
 # Detailed instructions how to build Qt from Source see:
@@ -27,32 +32,47 @@
 # - Python 3.x from https://www.python.org/downloads/
 # - CMake >= 3.15 from https://cmake.org/download/
 # - jom.exe (nmake replacement) from https://wiki.qt.io/Jom
+#
+# Qt6
+#=====
+# Prerequisites to build Qt on Windows see:
+#    https://doc.qt.io/qt-6/windows.html
+# Detailed instructions how to build Qt from Source see:
+#    https://doc.qt.io/qt-6/windows-building.html
+#
+# Minumum requirements to sucessfully execute this script:
+# - A recent ActivePerl installation from https://www.activestate.com/activeperl
+# - Python 3.x from https://www.python.org/downloads/
+# - CMake >= 3.17 from https://cmake.org/download/
+# - ninja.exe (nmake replacement) from https://ninja-build.org/
 
-#Set all platforms to be build (Supported: Win32, x64)
-platforms="Win32 x64"
 # Appropriate Visual Studio versions and types:
-vsversions="2019 2017"
+vsversions="2019 2022"
 vstypes="Enterprise Professional Community"
 
 # DO NOT CHANGE ANYTHING BEYOND THIS LINE
 #========================================
 
 function usage() {
-    echo "Download and rebuild Qt 5.x.y libraries needed to build flexemu"
+    echo "Download and rebuild Qt x.y.z libraries needed to build flexemu"
     echo ""
     echo "Syntax:"
-    echo "   download_and_rebuild_libs_qt5.sh [-d][-V <vs_version][-T <vs_type>]"
-    echo "                                    -v <qt_version>"
+    echo "   download_and_rebuild_libs.sh [-d][-V <vs_version][-T <vs_type>]"
+    echo "                                -v <qt_version>"
     echo "Options:"
     echo "   -d:             Delete Qt downloads and build directories before downloading"
     echo "                   and building them"
-    echo "   -V <vs_version> The Visual Studio version, 2017 or 2019."
+    echo "   -V <vs_version> The Visual Studio version, 2019 or 2022."
     echo "                   If not set the script looks for an installed VS version"
-    echo "                   2019 or 2017, in this order."
+    echo "                   2019 or 2022, in this order. Visual Studio has to be"
+    echo "                   installed in the default path."
     echo "   -T <vs_type>    The Visual Studio type, Enterprise, Professional or"
     echo "                   Community. If not set the script looks for an installed VS"
     echo "                   type Enterprise, Professional or Community, in this order."
-    echo "   -v <qt_version> Specify Qt version to be build. Syntax: 5.<minor>.<patch>"
+    echo "   -v <qt_version> Specify Qt version to be build."
+    echo "                   Syntax: <major>.<minor>.<patch>"
+    echo "                   Supported major version is 5 or 6. See"
+    echo "                   https://download.qt.io/archive/qt/ for available versions."
 }
 
 qtversion=
@@ -70,7 +90,8 @@ do
                 qtversion=$2
                 shift
             else
-                echo "Error: Argument for $1 is missing" >&2
+                echo "**** Error: Argument for $1 is missing" >&2
+                usage
                 exit 1
             fi;;
         -V)
@@ -78,7 +99,8 @@ do
                 vsversion=$2
                 shift
             else
-                echo "Error: Argument for $1 is missing" >&2
+                echo "**** Error: Argument for $1 is missing" >&2
+                usage
                 exit 1
             fi;;
         -T)
@@ -86,7 +108,8 @@ do
                 vstype=$2
                 shift
             else
-                echo "Error: Argument for $1 is missing" >&2
+                echo "**** Error: Argument for $1 is missing" >&2
+                usage
                 exit 1
             fi;;
         *) break;;
@@ -95,18 +118,22 @@ do
 done
 
 if [ "x$qtversion" == "x" ]; then
-    echo "Error: Qt version has to be specified with -v 5.<minor>.<patch>." >&2
+    echo "**** Error: Qt version has to be specified with"
+    echo "**** -v <major>.<minor>.<patch>." >&2
     usage
     exit 1
 fi
-match=`echo $qtversion | sed -n "s/^\([5]\+\.[0-9]\+\.[0-9]\+\)$/\1/p"`
+match=`echo $qtversion | sed -n "s/^\([56]\.[0-9]\+\.[0-9]\+\)$/\1/p"`
 if [ "x$match" == "x" ]; then
-    echo "Error: Qt version '$qtversion' has invalid syntax. Must be 5.<minor>.<patch>" >&2
+    echo "**** Error: Qt version '$qtversion' has invalid syntax."
+    echo "**** Must be <5|6>.<minor>.<patch>" >&2
     usage
     exit 1
 fi
 qtversion=$match
-qtmamiversion=`echo $qtversion | sed -e "s/\([0-9]\+\.[0-9]\+\).*/\1/"`
+qtmamiversion=`echo $qtversion | sed -e "s/\([56]\.[0-9]\+\).*/\1/"`
+qtmaversion=`echo $qtversion | sed -e "s/\([56]\).*/\1/"`
+qtpatch=`echo $qtversion | sed -e "s/^[56]\.[0-9]\+\.\([0-9]\+\)$/\1/"`
 
 check_value() {
     if [ "x$1" == "x" ]; then
@@ -121,18 +148,28 @@ check_value() {
     return 1
 }
 
+# Get the base path of the Microsoft Visual Studio installation.
+# $1: Microsoft Visual Studio version ($vsversion)
+get_vsbasedir() {
+    if [ "$1" == "2019" ]; then
+        echo "C:\Program Files (x86)\Microsoft Visual Studio"
+    else
+        echo "C:\Program Files\Microsoft Visual Studio"
+    fi
+}
+
 # Look for an appropriate Visual Studio installation to set all needed
 # variables:
 check_value "$vsversion" "$vsversions"
 if [ $? -ne 0 ]; then
-    echo "*** Error: Visual Studio version \"$vsversion\" is not supported."
+    echo "**** Error: Visual Studio version \"$vsversion\" is not supported."
     usage
     exit 1
 fi
 
 check_value "$vstype" "$vstypes"
 if [ $? -ne 0 ]; then
-    echo "*** Error: Visual Studio type \"$vstype\" is not supported."
+    echo "**** Error: Visual Studio type \"$vstype\" is not supported."
     usage
     exit 1
 fi
@@ -144,7 +181,8 @@ if [ "x$vstype" == "x" ]; then
         do
             for vstype in $vstypes
             do
-                msvcscript="C:\Program Files (x86)\Microsoft Visual Studio\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
+                vsbasedir=$( get_vsbasedir $vsversion )
+                msvcscript="$vsbasedir\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
                 if [ -f "$msvcscript" ]; then
                     break
                 fi
@@ -157,7 +195,8 @@ if [ "x$vstype" == "x" ]; then
         # vsversion specified, look for vstype.
         for vstype in $vstypes
         do
-            msvcscript="C:\Program Files (x86)\Microsoft Visual Studio\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
+            vsbasedir=$( get_vsbasedir $vsversion )
+            msvcscript="$vsbasedir\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
             if [ -f "$msvcscript" ]; then
                 break
             fi
@@ -168,27 +207,38 @@ else
         # vstype specified, look for vsversion.
         for vsversion in $vsversions
         do
-            msvcscript="C:\Program Files (x86)\Microsoft Visual Studio\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
+            vsbasedir=$( get_vsbasedir $vsversion )
+            msvcscript="$vsbasedir\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
             if [ -f "$msvcscript" ]; then
                 break
             fi
         done
     else
         # both vstype and vsversion specified.
-        msvcscript="C:\Program Files (x86)\Microsoft Visual Studio\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
+        vsbasedir=$( get_vsbasedir $vsversion )
+        msvcscript="$vsbasedir\\$vsversion\\$vstype\VC\Auxiliary\Build\vcvarsall.bat"
     fi
 fi
+
+vsversion_file="vsversion.ini"
+echo "VS_VERSION=$vsversion" > $vsversion_file
+echo "VS_TYPE=$vstype" >> $vsversion_file
 
 if [ -f "$msvcscript" ]; then
     echo "Found Visual Studio $vsversion $vstype"
 else
-    echo "*** Error: No appropriate Visual Studio Installation found."
+    echo "**** Error: No appropriate Visual Studio Installation found."
+    echo "**** Microsoft Visual Studio has to be installed in the default path."
     usage
     exit 1
 fi
 
 # Create the url from which to download a specific version (Supported: Qt5.minor.patch)
-qturl=`echo "https://download.qt.io/archive/qt/${qtmamiversion}/${qtversion}/submodules/qtbase-everywhere-src-${qtversion}.zip"`
+qtos=""
+if [ "$qtmamiversion" == "5.15" ] && [ $qtpatch -ge 3 ]; then
+    qtos="-opensource"
+fi
+qturl=`echo "https://download.qt.io/archive/qt/${qtmamiversion}/${qtversion}/submodules/qtbase-everywhere${qtos}-src-${qtversion}.zip"`
 
 MSBUILDDISABLENODEREUSE=1
 export MSBUILDDISABLENODEREUSE
@@ -227,48 +277,68 @@ as_doublebslash_windows_path() {
 # $2: Architecture (x86 or amd64)
 # $3: Root path of the Qt source distribution (absolute Windows path)
 # $4: Path Qt build directory (absolute Windows path)
-# $5: Path of the generated batch script
+# $5: Qt major version
+# $6: Path of the generated batch script
 create_qt_build_script() {
-    echo CALL \"$1\" $2 >$5
-    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$5
-    echo SET _ROOT=$3 >>$5
-    echo SET PATH=\%_ROOT\%\\bin\;\%PATH\% >>$5
-    echo SET _ROOT= >>$5
-    echo >>$5
-    echo cd \"$4\" >>$5
-    echo CALL \"$3\\configure.bat\" -redo >>$5
-    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$5
-    echo jom.exe >>$5
-    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$5
-    echo jom.exe install >>$5
-    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$5
+    echo CALL \"$1\" $2 >$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+    echo SET _ROOT=$3 >>$6
+    echo SET PATH=\%_ROOT\%\\bin\;\%PATH\% >>$6
+    echo SET _ROOT= >>$6
+    echo >>$6
+    echo cd \"$4\" >>$6
+    echo CALL \"$3\\configure.bat\" -redo >>$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+if [ "$5" = "5" ]; then
+    echo jom.exe >>$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+    echo jom.exe install >>$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+else
+    echo cmake --build . --parallel >>$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+    echo ninja.exe install >>$6
+    echo IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL% >>$6
+fi
 }
 
 # Create the Qt build config file.
 # $1: The target directory where the build artefacts are copied
-# $2: Path to the config file
+# $2: Qt major version
+# $3: Path to the config file
+# Remark: Some more features have been activated to reduce the
+# chance of an internal compiler error.
 create_config_file() {
-    echo "-platform" > $2
-    echo "win32-msvc2019" >> $2
-    echo "-prefix" >> $2
-    echo "$1" >>$2
-    echo "-debug-and-release" >>$2
-    echo "-no-feature-network" >>$2
-    echo "-no-feature-sql" >>$2
-    echo "-no-feature-testlib" >>$2
-    echo "-no-feature-concurrent" >>$2
-    echo "-no-feature-dbus" >>$2
-    echo "-no-feature-xml" >>$2
-    echo "-nomake" >>$2
-    echo "examples" >>$2
-    echo "-qt-zlib" >>$2
-    echo "-no-harfbuzz" >>$2
-    echo "-no-opengl" >>$2
-    echo "-c++std" >>$2
-    echo "c++11" >>$2
-    echo "-mp" >>$2
-    echo "-confirm-license" >>$2
-    echo "-opensource" >>$2
+    echo "-platform" > $3
+    echo "win32-msvc" >> $3
+    echo "-prefix" >> $3
+    echo "$1" >>$3
+    echo "-debug-and-release" >>$3
+    echo "-feature-network" >>$3
+    echo "-feature-sql" >>$3
+    echo "-no-feature-testlib" >>$3
+    echo "-feature-concurrent" >>$3
+    echo "-feature-dbus" >>$3
+    echo "-feature-xml" >>$3
+    echo "-nomake" >>$3
+    echo "examples" >>$3
+    echo "-qt-zlib" >>$3
+    echo "-qt-harfbuzz" >>$3
+    echo "-opengl" >>$3
+    echo "desktop" >>$3
+if [ "$2" = "6" ]; then
+    # Avoid internal compiler error on Qt6
+    echo "-no-pch" >>$3
+fi
+    echo "-c++std" >>$3
+if [ "$2" = "5" ]; then
+    echo "c++11" >>$3
+else
+    echo "c++17" >>$3
+fi
+    echo "-mp" >>$3
+    echo "-confirm-license" >>$3
+    echo "-opensource" >>$3
 }
 
 curlpath=`which curl 2>/dev/null`
@@ -278,6 +348,26 @@ if [ "x$curlpath" = "x" ]; then
     echo "  https://curl.haxx.se/download.html"
     echo "  The executable has to be copied into the git installation"
     echo "  mingw32/bin or mingw64/bin."
+fi
+
+if [ "$qtmaversion" = "5" ]; then
+    jompath=`which jom 2>/dev/null`
+    if [ "x$jompath" = "x" ]; then
+        echo "**** Error: jom not found."
+        echo "  Jom can be downloaded from"
+        echo "  https://wiki.qt.io/Jom"
+        echo "  The executable has to be copied into PATH."
+        exit 1
+    fi
+else
+    ninjapath=`which ninja 2>/dev/null`
+    if [ "x$ninjapath" = "x" ]; then
+        echo "**** Error: ninja not found."
+        echo "  ninja can be downloaded from"
+        echo "  https://ninja-build.org/"
+        echo "  The executable has to be copied into PATH."
+        exit 1
+    fi
 fi
 
 urls=`echo "$qturl"`
@@ -292,6 +382,7 @@ if [ "$delete" = "yes" ]; then
     for url in $urls
     do
         file=$(basename "$url")
+        file=`echo "$file" | sed -e "s/\(.\+\)-opensource\(.\+\)/\1\2/"`
         if [ -r $qtdir/$file ]; then
             echo deleting file $qtdir/$file...
             rm -f $qtdir/$file
@@ -324,7 +415,7 @@ do
     fi
 done
 if [ ! "$?" == "0" ]; then
-    echo "Error: Download failed. Aborted." >&2
+    echo "**** Error: Download failed. Aborted." >&2
     rm -f $qtdir/$file
     exit 1
 fi
@@ -334,8 +425,9 @@ fi
 qtsrcdir=
 for url in $urls
 do
-    file=$(basename "$url")
-    if [ -r $qtdir/$file ]; then
+    orgfile=$(basename "$url")
+    file=`echo "$orgfile" | sed -e "s/\(.\+\)-opensource\(.\+\)/\1\2/"`
+    if [ -r $qtdir/$orgfile ]; then
         extension=`echo "$file" | sed -e "s/.\+\(zip\|tar.gz\)/\1/"`
         filebase=`echo "$file" | sed -e "s/\(.\+\)\.\(zip\|tar.gz\)/\1/"`
         directory=$filebase
@@ -343,13 +435,13 @@ do
             qtsrcdir=$qtdir/$directory
         fi
         if [ ! -d $qtdir/$directory ]; then
-            echo unpacking $file into $qtdir...
+            echo unpacking $orgfile into $qtdir...
             case "$extension" in
-                tar.gz) tar -C $qtdir xfz $qtdir/$file ;;
-                zip) unzip -q $qtdir/$file -d $qtdir ;;
+                tar.gz) tar -C $qtdir xfz $qtdir/$orgfile ;;
+                zip) unzip -q $qtdir/$orgfile -d $qtdir ;;
             esac
             if [ ! "$?" == "0" ]; then
-                echo "Error: Unpacking file failed. Aborted." >&2
+                echo "**** Error: Unpacking file failed. Aborted." >&2
                 exit 1
             fi
             # Wait for 2 seconds otherwise the mv could fail
@@ -368,6 +460,16 @@ fi
 
 absqttgtdir=$( as_absolute_windows_path $qtdir/$tgtdir )
 
+# Get all supported platforms to be build (Supported: Win32, x64).
+# They depend on the Qt major version
+if [ "$qtmaversion" == "5" ]; then
+    # Supported on Qt5: Win32, x64
+    platforms="Win32 x64"
+else
+    # Supported on Qt6: x64
+    platforms="x64"
+fi
+
 # Create batch script to build Qt libraries for
 # all requested platforms and execute it.
 for platform in $platforms
@@ -382,16 +484,16 @@ do
     fi
 
     configoptpath=$qtdir/$directory/config.opt
-    create_config_file $absqttgtdir\\$platform $configoptpath
+    create_config_file $absqttgtdir\\$platform $qtmaversion $configoptpath
     absqtsrcdir=$( as_absolute_windows_path $qtsrcdir )
     absqtbuilddir=$( as_absolute_windows_path $qtdir/$directory )
     batchscript=$qtdir/$directory/build_qt.bat
-    create_qt_build_script "$msvcscript" $arch "$absqtsrcdir" "$absqtbuilddir" $batchscript
+    create_qt_build_script "$msvcscript" $arch "$absqtsrcdir" "$absqtbuilddir" $qtmaversion $batchscript
 
     echo building $platform Qt $qtversion libraries...
     cmd < $batchscript
     if [ ! "$?" == "0" ]; then
-        echo "Error: Building Qt libraries. Aborted." >&2
+        echo "**** Error: Building Qt libraries. Aborted." >&2
         exit 1
     fi
 done
--- build/windows/rebuild_flexemu_all_and_copy_libs.sh
+++ build/windows/rebuild_flexemu_all_and_copy_libs.sh
@@ -1,31 +1,91 @@
 #!/usr/bin/sh
 #
+# Rebuild flexemu and copy all needed libraries into the build folder.
+#
+# Before calling this script download_and_rebuild_libs.sh has to
+# be executed before to create the Qt libraries and prepare the
+# Visual Studio flexemu solution.
+#
 # syntax:
 #    rebuild_flexemu_all_and_copy_libs.sh
 # parameters:
 #    none
 #
-if [ "x$VS160COMNTOOLS" = "x" ]; then
-    # If not defined use the default path of the VS2019 community edition.
-    # This path may evtl. has to be adapted.
-    VS160COMNTOOLS="C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\Tools\\"
+#
+# Convert an absolute path with Windows syntax into a path
+# with MINGW syntax.
+as_mingw_path() {
+    expr='s:\\:/:g'
+    path=`echo $1 | sed $expr`
+    drive=`echo $path | sed -ne "s:\(.\).*:/\L\1:p"`
+    path=`echo $path | sed "s/^..//"`
+    echo ${drive}${path}
+}
+
+# Get the base path of the Microsoft Visual Studio installation.
+# $1: Microsoft Visual Studio version ($vsversion)
+get_vsbasedir() {
+    if [ "$1" == "2019" ]; then
+        echo "C:\Program Files (x86)\Microsoft Visual Studio"
+    else
+        echo "C:\Program Files\Microsoft Visual Studio"
+    fi
+}
+
+
+# Read Microsoft Visual Studio Version and type from file
+vsversion_file="vsversion.ini"
+if [ ! -f $vsversion_file ]; then
+    echo "**** File $vsversion_file does not exist."
+    echo "**** First call download_and_rebuild_libs.sh to create libraries!"
+    exit 1
 fi
 
-# Path of Qt5 build.
-# This path may evtl. has to be adapted.
-QTDIR=/c/Qt/Qt5.15
+vsversion=`sed -ne "s/VS_VERSION\s*=\s*\(.*\)/\1/p" $vsversion_file`
+vstype=`sed -ne "s/VS_TYPE\s*=\s*\(.*\)/\1/p" $vsversion_file`
+if [ "x$vsversion" = "x" ] || [ "x$vstype" = "x" ]; then
+    echo "**** Can't read Microsoft Visual Studio version or type."
+    echo "**** Check file $vsversion_file. It should have a line"
+    echo "**** VS_VERSION=... and VS_TYPE=..."
+    exit 1
+fi
+vsbasedir=$( get_vsbasedir $vsversion )
+
+VS160COMNTOOLS="$vsbasedir\\$vsversion\\$vstype\\Common7\\Tools\\"
+if [ ! -d "$VS160COMNTOOLS" ]; then
+    echo "**** Microsoft Visual Studio path not found:"
+    echo "**** $VS160COMNTOOLS"
+    echo "**** Check Your Microsoft Visual Studio installation."
+    exit 1
+fi
+echo "Using Visual Studio $vsversion $vstype"
+
+# Path of Qt build.
+# Parse it from the *.props file.
+propsfile="../../src/msvcQtPath.props"
+QTDIR=`sed -ne "s/<QTDIR>\(.*\)<\/QTDIR>/\1/p" $propsfile`
+QTDIR=$( as_mingw_path $QTDIR)
+qtversion=`echo $QTDIR | sed -e "s/.*\([56]\.[0-9]\+\.[0-9]\+\)$/\1/"`
+qtmaversion=`echo $qtversion | sed -e "s/\([56]\).*/\1/"`
 
 create_rebuild_script() {
     echo \@echo off >$2
     echo set DevEnvDir=$VS160COMNTOOLS\..\\IDE >>$2
+    echo echo ... rebuild Release Win32 >>$2
     echo \"\%DevEnvDir%\\devenv.exe\" $1 /Rebuild \"Release\|Win32\" >>$2
+    echo echo ... rebuild Release x64 >>$2
     echo \"\%DevEnvDir%\\devenv.exe\" $1 /Rebuild \"Release\|x64\" >>$2
+    echo echo ... rebuild Debug Win32 >>$2
     echo \"\%DevEnvDir%\\devenv.exe\" $1 /Rebuild \"Debug\|Win32\" >>$2
+    echo echo ... rebuild Debug x64 >>$2
     echo \"\%DevEnvDir%\\devenv.exe\" $1 /Rebuild \"Debug\|x64\" >>$2
 }
 
-if [ ! -d $QTDIR/x64/bin ]; then
-    echo "**** First call download_and_rebuild_libs.sh to create libraries!"
+temp=$QTDIR/x64/bin
+if [ ! -d $temp ]; then
+    echo "**** Qt libraries build directory does not exist:"
+    echo "**** $temp"
+    echo "**** First call download_and_rebuild_libs.sh to create QT libraries!"
     exit 1
 fi
 
@@ -53,7 +113,12 @@ do
         if [ ! -d $targetdir ]; then
             mkdir $targetdir
         fi
-        for file in Qt5Core Qt5Gui Qt5Widgets
+        if [ "$qtmaversion" = "5" ]; then
+            qtlibs="Qt5Core Qt5Gui Qt5Widgets"
+        else
+            qtlibs="Qt6Core Qt6Gui Qt6Widgets"
+        fi
+        for file in $qtlibs
         do
             cp -f ${QTDIR}/${platform}/bin/${file}${postfix}.dll $targetdir
         done
