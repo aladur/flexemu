9fe8384a2f32329b045013ed18ff6fa8a5396866
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2023-11-25 20:46:17 +0100

    Limit Linux header to Linux platform.

--- src/misc1.cpp
+++ src/misc1.cpp
@@ -28,8 +28,10 @@
 #include <functional>
 #include <sys/types.h>
 #include <sys/stat.h>
-#ifdef UNIX
+#ifdef __LINUX
 #include <linux/param.h>
+#endif
+#ifdef UNIX
 #include <netdb.h>
 #include <pwd.h>
 #endif
