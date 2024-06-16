2b0be33b38ef56ff0cf8f44ba94ba58da57b3f73
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2024-06-16 13:59:06 +0200

    Add ci_string, a std::string with case insensitive comparison.
    - A specialized ci_char_traits and ci_string is defined, supporting case
      insensitive comparison with ci_string::compare() member function.
    - See also:
      http://www.gotw.ca/gotw/029.htm

--- src/misc1.h
+++ src/misc1.h
@@ -509,5 +509,59 @@ template<size_t N> std::string getstr(const char (&array)[N])
 
     return result;
 }
+
+// Define a case insensitive char trait.
+// Together with a ci_string type definition it allows a
+// case insensitive string comparison with the
+// ci_string.compare() member function.
+struct ci_char_traits : public std::char_traits<char>
+{
+    static bool eq(char c1, char c2)
+    {
+        return tolower(c1) == tolower(c2);
+    }
+
+    static bool ne(char c1, char c2)
+    {
+        return tolower(c1) != tolower(c2);
+    }
+
+    static bool lt(char c1, char c2)
+    {
+        return tolower(c1) < tolower(c2);
+    }
+
+    static int compare(const char* c1, const char* c2, size_t n)
+    {
+        while (n-- != 0)
+        {
+            if (tolower(*c1) < tolower(*c2))
+            {
+                return -1;
+            }
+            if (tolower(*c1) > tolower(*c2))
+            {
+                return 1;
+            }
+            ++c1;
+            ++c2;
+        }
+        return 0;
+    }
+
+    static const char* find(const char* s, int n, char a)
+    {
+        const auto lc_a = tolower(a);
+
+        while (n-- > 0 && tolower(*s) != lc_a)
+        {
+            ++s;
+        }
+        return s;
+    }
+};
+
+typedef std::basic_string<char, ci_char_traits> ci_string;
+
 #endif /* __misc1.h__ */
 
