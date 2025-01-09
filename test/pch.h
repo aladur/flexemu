1dcb96d857fc1a1d3438ef130431f010ec5a4bf5
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2025-01-09 20:34:18 +0100

    automake: test uses it's own pch.h file.
    - Use ALL_TARGETS_CXXFLAGS for all targets to make sure that the
      precompiled header uses the identical compiler flags.
    - Also: Do not set pch.h.gch as dependency instead use BUILT_SOURCES.
    - Also: Avoid compile error: __OPTIMIZE__ macro was disabled in PCH
      file but is currently enabled.
      Solution: Add $(CXXFLAGS) when creating precompiled header.
    - Also: Add test/pch.h and test/pch.cpp to EXTRA_DIST to avoid breaking
      distcheck.

---
+++ test/pch.h
@@ -0,0 +1,42 @@
+/*
+    pch.h
+
+
+    flexemu, an MC6809 emulator running FLEX
+    Copyright (C) 2025  W. Schwotzer
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#ifndef PCH_INCLUDED
+#define PCH_INCLUDED
+
+#include "misc1.h"
+#include <memory>
+#include <functional>
+#include <cassert>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <array>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+#endif
+
