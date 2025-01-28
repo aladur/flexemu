c898bb2010cb040f3cec0101cb99a5a39c7f1ae1
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2025-01-28 19:12:35 +0100

    Add copyright message to test files.

--- test/fixt_filecont.h
+++ test/fixt_filecont.h
@@ -1,3 +1,26 @@
+/*
+    fixt_filecont.h
+
+
+    flexemu, an MC6809 emulator running FLEX
+    Copyright (C) 2024-2025  W. Schwotzer
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
 #include "gtest/gtest.h"
 #include "ffilecnt.h"
 #include "dircont.h"
